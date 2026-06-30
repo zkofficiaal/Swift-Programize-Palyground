d176c8d53a91605d236564acfa125cd61958f467
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-06-29 23:39:23 -0700

    adding new swift term

--- Contents.swift
+++ Contents.swift
@@ -3,3 +3,54 @@ import UIKit
 var greeting = "Hello, playground"
 // Hellow world programe
 print ("Hellow World!")
+
+
+
+// variable
+var num : Int
+var name : String
+
+//assign values to a variable
+var curren_month : String = "July"
+
+// change vales of a variable
+var today : String = "Tuesday"
+print (today)
+
+today = "Wednesday"
+print (today)
+
+//rules for naming swift variable, not allowed (e.g var $a, 3s)
+var a = 10
+var _a = 20
+var A = 30
+var nameOfStudent = "Zahid Khan"// camelCase
+
+
+
+
+// Constant
+let amount : Int = 70
+let currency = "$"
+
+
+
+//literals
+let siteName = "PirsoSoft"
+
+//integral literral
+let value : Int = 56
+
+//floating point literals
+let piValue = 3.141579
+
+//boolean literals
+let pass : Bool = true
+let isValue : Bool = false
+
+//character literal
+var alphabet : Character = "$"
+
+//string literal
+let officeName = "Piros Soft Office"
+
